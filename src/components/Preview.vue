a0feeea6008770532f8dcc07cfa0d90343356320
yuetsh <517252939@qq.com>
2025-02-24 12:19:32 +0800

    add normalize.css

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -19,6 +19,7 @@ function preview() {
     <title>预览</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>${css.value}</style>
+    <link rel="stylesheet" href="/normalize.min.css" />
   </head>
   <body>
     ${html.value}
